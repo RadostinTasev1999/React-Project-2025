b0a5ae388e4e2955124e81ae362d48056d1999aa
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-03-30 20:59:46 +0300

    Wrap App.js JSX in UseProvider component

--- client-app/src/App.jsx
+++ client-app/src/App.jsx
@@ -2,22 +2,30 @@ import Header from './components/header/Header'
 import './App.css'
 import { Routes,Route } from 'react-router'
 import Home from './components/home/Home'
+import Login from './components/login/Login'
+import Register from './components/register/Register'
+import UserProvider from './providers/UserProvider'
 
 function App() {
  
 
   return (
     <>
-      <div id="box">
-        <Header/>
+  <UserProvider>
+        <div id="box">
+          <Header />
 
-    <main id="main-content">
-        <Routes>
-            <Route index element={<Home />} />
-        </Routes>
-    </main>
+          <main id="main-content">
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Register />} />
+            </Routes>
+          </main>
 
-      </div>
+        </div>
+  </UserProvider>
+      
     </>
   )
 }
