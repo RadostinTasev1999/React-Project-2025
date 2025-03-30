bf783c9977c86fba90d74371b94b961110e7a5df
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-03-30 21:03:24 +0300

    create custom state hook

---
+++ client-app/src/hooks/usePersistedState.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+export default function usePersistedState(stateKey, inititalState) {
+
+    const [state,setState] = useState(() => {
+
+        const persistedState = localStorage.getItem(stateKey);
+
+        if (!persistedState) {
+            return typeof inititalState === 'function'
+                ?
+            inititalState()
+                :
+            inititalState
+        }
+
+        const persistedStateData = JSON.parse(persistedState)
+
+        return persistedStateData
+
+    });
+
+        const setPersistedState = (input) => {
+            
+            const data = typeof input === 'function'
+                    ?
+                input(state)
+                    :
+                  input;
+
+        const persistedData = JSON.stringify(data)
+
+        localStorage.setItem(stateKey, persistedData)
+
+
+        setState(persistedData)
+
+        };
+    
+   return [
+    state,
+    setPersistedState
+   ]
+}
