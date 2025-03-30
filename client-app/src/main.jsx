b2b2322682aff45f93effffc4962673ec8fef101
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-03-30 21:09:26 +0300

    use BrowserRouter to enable navigation within the App

--- client-app/src/main.jsx
+++ client-app/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
 
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+         <App />
+    </BrowserRouter>
+   
 )
+
+/*
+BrowserRouter is a component from React Router, a library used for handling routing in React applications.
+It enables navigation within a single-page application (SPA) without reloading the entire page.
+*/
