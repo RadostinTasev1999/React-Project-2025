60ea45150c78b801f7e4078b7cae54f1b488089e
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-03-31 21:27:54 +0300

    Update Header component

--- client-app/src/components/header/Header.jsx
+++ client-app/src/components/header/Header.jsx
@@ -1,7 +1,9 @@
 
-import { useContext, useState } from 'react'
-import { Link } from 'react-router'
+import {  useContext, useState } from 'react'
+import { Link, useNavigate } from 'react-router'
 import { UserContext } from '../../contexts/UserContext'
+
+//import useAuth from '../../hooks/useAuth'
 
 
 import {
@@ -44,7 +46,24 @@ const products = [
 export default function Header() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
     
-    const { accessToken } = useContext(UserContext)
+    const navigate = useNavigate()
+    //const { accessToken,...authData } = useContext(UserContext)
+
+    const { accessToken, ...authData } = useContext(UserContext)
+
+    console.log('Access token is:', accessToken)
+    //console.log('AuthData is:', ...authData)
+
+   const {userLogoutHandler} = useContext(UserContext)
+
+
+   const onLogout = () => {
+      userLogoutHandler()
+
+      navigate('/')
+
+   }
+
 
     return (
         <header className="bg-white">
@@ -70,55 +89,8 @@ export default function Header() {
               </button>
             </div>
             <PopoverGroup className="hidden lg:flex lg:gap-x-12">
-              <Popover className="relative">
-                <PopoverButton className="flex items-center gap-x-1 text-sm/6 font-semibold text-gray-900">
-                  Categories
-                  <ChevronDownIcon aria-hidden="true" className="size-5 flex-none text-gray-400" />
-                </PopoverButton>
-    
-                <PopoverPanel
-                  transition
-                  className="absolute top-full -left-8 z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white ring-1 shadow-lg ring-gray-900/5 transition data-closed:translate-y-1 data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-150 data-leave:ease-in"
-                >
-                  <div className="p-4">
-                    {products.map((item) => (
-                      <div
-                        key={item.name}
-                        className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm/6 hover:bg-gray-50"
-                      >
-                        <div className="flex size-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                          <item.icon aria-hidden="true" className="size-6 text-gray-600 group-hover:text-indigo-600" />
-                        </div>
-                        <div className="flex-auto">
-                          <Link to={item.to} className="block font-semibold text-gray-900">
-                            {item.name}
-                            <span className="absolute inset-0" />
-                          </Link>
-                          {/* <p className="mt-1 text-gray-600">{item.description}</p> */}
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                  <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-                    {callsToAction.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className="flex items-center justify-center gap-x-2.5 p-3 text-sm/6 font-semibold text-gray-900 hover:bg-gray-100"
-                      >
-                        <item.icon aria-hidden="true" className="size-5 flex-none text-gray-400" />
-                        {item.name}
-                      </a>
-                    ))}
-                  </div>
-                </PopoverPanel>
-              </Popover>
-    
-              <Link to="/features" className="text-sm/6 font-semibold text-gray-900">
-                Features
-              </Link>
-              <Link to="/news" className="text-sm/6 font-semibold text-gray-900">
-                News
+              <Link to="/posts" className="text-sm/6 font-semibold text-gray-900">
+                All Posts
               </Link>
               <Link to="/about" className="text-sm/6 font-semibold text-gray-900">
                 About
@@ -126,6 +98,17 @@ export default function Header() {
             </PopoverGroup>
             { accessToken
                   ?
+                  (
+              <div className="hidden lg:flex ml-8 lg:gap-x-4">
+                <Link to="/logout" onClick={onLogout} className="text-sm/6 font-semibold text-blue-500">
+                  Log out
+                </Link>
+                <Link to="/create" className="text-sm/6 font-semibold text-blue-500">
+                    Create Post
+                </Link>
+              </div>
+                  )
+                  :
                   (
               <div className="hidden lg:flex ml-8 lg:gap-x-4">
                 <Link to="/login" className="text-sm/6 font-semibold text-blue-500">
@@ -136,14 +119,6 @@ export default function Header() {
                   Register
                 </Link>
               </div>
-                  )
-                  :
-                  (
-                 <div className="hidden lg:flex ml-8 lg:gap-x-4">
-                   <Link to="/logout" className="text-sm/6 font-semibold text-blue-500">
-                      Log out
-                   </Link>
-                </div>
                   )
             
 
