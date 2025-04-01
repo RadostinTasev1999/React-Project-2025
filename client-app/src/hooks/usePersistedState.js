2215df85f42406b19c9d599e6c334ad9014509a4
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-04-01 21:22:55 +0300

    update state property in setter callback function

--- client-app/src/hooks/usePersistedState.js
+++ client-app/src/hooks/usePersistedState.js
@@ -32,12 +32,14 @@ export default function usePersistedState(stateKey, inititalState) {
                   input;
 
         const persistedData = JSON.stringify(data)
-
+        console.log(persistedData)
         localStorage.setItem(stateKey, persistedData)
 
         console.log('persistedData is:', persistedData)
-        
-        setState(persistedData)
+        /*
+        {"email":"peter@abv.bg","username":"Peter","_id":"35c62d76-8152-4626-8712-eeb96381bea8","accessToken":"df495dd226a6e4c7c42c53158f3da6d5bcc98686c86bb2bdf8e82be5dd024b07"}
+        */
+        setState(data)
 
         };
     
