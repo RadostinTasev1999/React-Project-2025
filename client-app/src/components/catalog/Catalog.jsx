7c0138c798439480ead98ae33c8feb55a5b310b6
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-04-01 21:29:12 +0300

    Added route to PostDetails component

--- client-app/src/components/catalog/Catalog.jsx
+++ client-app/src/components/catalog/Catalog.jsx
@@ -1,6 +1,6 @@
 // import { usePosts } from "../../api/postApi"
 import { formattedDate } from "../../utils/date"
-import useAuth from "../../hooks/useAuth"
+
 import { Link } from "react-router"
 
 import { usePosts } from "../../api/postApi"
@@ -8,7 +8,7 @@ import { usePosts } from "../../api/postApi"
 export default function Catalog() {
 
     const {posts } = usePosts()
-    const { email } = useAuth()
+    // const { email } = useAuth()
 
    
     console.log('Posts are:', posts)
@@ -21,36 +21,33 @@ export default function Catalog() {
                 <div className="mx-auto max-w-7xl px-6 lg:px-8">
                     <div className="mx-auto max-w-2xl lg:mx-0">
                         <h2 className="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">Latest posts</h2>
-                        <p className="mt-2 text-lg/8 text-gray-600">Learn the latest features and news from the community hub.</p>
+                        <p className="mt-2 text-lg/8 text-gray-600">Learn the latest product feedback and features from the community hub.</p>
                     </div>
                     <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
                         {posts.map((post) => (
-                            <article key={post._id} className="flex max-w-xl flex-col items-start justify-between">
+                            <article key={post._id} className="flex max-w-xl flex-col items-start justify-between border-4 border-gray-300 rounded-xl p-6 shadow-lg bg-white">
                                 <div className="flex items-center gap-x-4 text-xs">
                                     <time dateTime={formattedDate(posts._createdOn)} className="text-gray-500">
                                         {formattedDate(posts._createdOn)}
                                     </time>
-                                    <a
-                                        href="#"
-                                        className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-                                    >
-                                        {email}
-                                    </a>
+                                    
                                 </div>
                                 <div className="group relative">
                                     <h3 className="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-gray-600">
-                                        <a href="#">
+                                        <Link to={`/posts/${post._id}/details`}>
                                             <span className="absolute inset-0" />
                                             {post.title}
-                                        </a>
+                                        </Link>
                                     </h3>
                                     <p className="mt-5 line-clamp-3 text-sm/6 text-gray-600">{post.description}</p>
                                 </div>
                                 <div className="relative mt-8 flex items-center gap-x-4">
                                     <img alt="Image" src={post.image} className="size-50  w-82 h-32" />
-                                    <Link to={`/posts/${post._id}/details`}className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400">More info</Link>
                                 </div>
+                                <Link to={`/posts/${post._id}/details`}className="px-4 py-1.5 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 mt-2">See post</Link>
+
                             </article>
+                            
                         ))}
                     </div>
                 </div>
