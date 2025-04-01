5ae4ef46692abb3b6e7c2d0b330c34efa1dc0e25
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-04-01 21:25:48 +0300

    grant access to accessToken and add route to home page

--- client-app/src/components/header/Header.jsx
+++ client-app/src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 
-import {  useContext, useState } from 'react'
+import {  useContext } from 'react'
 import { Link, useNavigate } from 'react-router'
 import { UserContext } from '../../contexts/UserContext'
 
@@ -31,28 +31,22 @@ import {
 import { ChevronDownIcon, PhoneIcon, PlayCircleIcon,DeviceTabletIcon,SpeakerWaveIcon } from '@heroicons/react/20/solid'
 //import { UserContext } from '../../contexts/UserContext'
 
-const products = [
-    { name: 'Headsets', to: '/headsets', icon: SpeakerWaveIcon },
-    { name: 'Teams panels', to: '/panels', icon: DeviceTabletIcon },
-    { name: 'Speakerphones', to: '/speakerphones', icon: SpeakerWaveIcon },
-    { name: 'Desk phones', to: '/desk-phones', icon: PhoneIcon },
-    { name: 'Teams Rooms', to: '/teams-rooms', icon: DeviceTabletIcon },
-  ]
-  const callsToAction = [
-    { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
-    { name: 'Contact sales', href: '#', icon: PhoneIcon },
-  ]
+
 
 export default function Header() {
-    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+    
     
     const navigate = useNavigate()
-    //const { accessToken,...authData } = useContext(UserContext)
+    //const { accessToken,...accessToken } = useContext(UserContext)
 
-    const { accessToken, ...authData } = useContext(UserContext)
+    const { accessToken } = useContext(UserContext)
+
+   
+
+
 
     console.log('Access token is:', accessToken)
-    //console.log('AuthData is:', ...authData)
+    //console.log('accessToken is:', ...accessToken)
 
    const {userLogoutHandler} = useContext(UserContext)
 
@@ -69,8 +63,8 @@ export default function Header() {
         <header className="bg-white">
           <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
             <div className="flex lg:flex-1">
-              <Link to={products.to} className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
+              <Link to="/" className="-m-1.5 p-1.5">
+                <span className="sr-only">Teams Devices Forum</span>
                 <img
                   alt=""
                   src="https://redmondmag.com/-/media/ECG/redmondmag/Images/introimages/1211red_teamslogo.jpg"
@@ -78,34 +72,26 @@ export default function Header() {
                 />
               </Link>
             </div>
-            <div className="flex lg:hidden">
-              <button
-                type="button"
-                onClick={() => setMobileMenuOpen(true)}
-                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-              >
-                <span className="sr-only">Open main menu</span>
-                <Bars3Icon aria-hidden="true" className="size-6" />
-              </button>
-            </div>
+            
             <PopoverGroup className="hidden lg:flex lg:gap-x-12">
-              <Link to="/posts" className="text-sm/6 font-semibold text-gray-900">
+              <Link to="/posts" className="text-sm/6 font-semibold text-blue-500">
                 All Posts
               </Link>
-              <Link to="/about" className="text-sm/6 font-semibold text-gray-900">
-                About
-              </Link>
+              
             </PopoverGroup>
             { accessToken
                   ?
                   (
               <div className="hidden lg:flex ml-8 lg:gap-x-4">
-                <Link to="/logout" onClick={onLogout} className="text-sm/6 font-semibold text-blue-500">
+                <Link onClick={onLogout} className="text-sm/6 font-semibold text-blue-500">
                   Log out
                 </Link>
                 <Link to="/create" className="text-sm/6 font-semibold text-blue-500">
                     Create Post
                 </Link>
+                <Link to="/admin" className="text-sm/6 font-semibold text-blue-500">
+                Admin
+              </Link>
               </div>
                   )
                   :
@@ -124,53 +110,7 @@ export default function Header() {
 
             }
           </nav>
-          <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-            <div className="fixed inset-0 z-10" />
-            <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-              <div className="flex items-center justify-between">
-                <a href="#" className="-m-1.5 p-1.5">
-                  <span className="sr-only">Your Company</span>
-                  <img
-                    alt=""
-                    src="https://redmondmag.com/-/media/ECG/redmondmag/Images/introimages/1211red_teamslogo.jpg"
-                    className="h-8 w-auto"
-                  />
-                </a>
-                <button
-                  type="button"
-                  onClick={() => setMobileMenuOpen(false)}
-                  className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                >
-                  <span className="sr-only">Close menu</span>
-                  <XMarkIcon aria-hidden="true" className="size-6" />
-                </button>
-              </div>
-              <div className="mt-6 flow-root">
-                <div className="-my-6 divide-y divide-gray-500/10">
-                  <div className="space-y-2 py-6">
-                    <Disclosure as="div" className="-mx-3">
-                      <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
-                        Product
-                        <ChevronDownIcon aria-hidden="true" className="size-5 flex-none group-data-open:rotate-180" />
-                      </DisclosureButton>
-                      <DisclosurePanel className="mt-2 space-y-2">
-                        {[...products, ...callsToAction].map((item) => (
-                          <DisclosureButton
-                            key={item.name}
-                            as="a"
-                            href={item.href}
-                            className="block rounded-lg py-2 pr-3 pl-6 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50"
-                          >
-                            {item.name}
-                          </DisclosureButton>
-                        ))}
-                      </DisclosurePanel>
-                    </Disclosure>
-                  </div>   
-                </div>
-              </div>
-            </DialogPanel>
-          </Dialog>
+          
         </header>
       )
     }
