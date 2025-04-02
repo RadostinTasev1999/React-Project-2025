aa573906c5cf73080fcd639d75870bc91eaef74e
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-04-02 23:33:30 +0300

    Added styles to div and main elements and added route to PrivacyPolicy component

--- client-app/src/App.jsx
+++ client-app/src/App.jsx
@@ -12,56 +12,57 @@ import EditPost from './components/edit/EditPost'
 import CommentsEdit from './components/comments-edit/CommentEdit'
 import AuthGuard from './components/guards/AuthGuard'
 import GuestGuard from './components/guards/GuestGuard'
+import Footer from './components/footer/Footer'
 
 import { Suspense, lazy } from 'react'
+import PrivacyPolicy from './components/policy/PrivacyPolicy'
 
 const Admin = lazy(() => import('./components/admin/Admin'))
-
-// dynamic import that loads the Admin component 
-// const Admin - this stores the dynamically imported component in the Admin constant
-//
-
-
 
 function App() {
  
 
   return (
     <>
-  <UserProvider>
-        <div id="box">
+      <UserProvider>
+        <div id="box" className='flex flex-col min-h-screen'>
           <Header />
 
-          <main id="main-content">
+          <main id="main-content" className='flex-grow'>
             <Routes>
+
               <Route index element={<Home />} />
               <Route path='/posts' element={<Catalog />} />
               <Route path='/posts/:postId/details' element={<PostDetails />} />
-              
-            <Route element={<AuthGuard />} >
+              <Route path='/policy' element={<PrivacyPolicy />} />
+
+              <Route element={<AuthGuard />} >
                 <Route path='/create' element={<Create />} />
                 <Route path='/posts/:postId/edit' element={<EditPost />} />
                 <Route path='/posts/:postId/comment/:commentId' element={<CommentsEdit />} />
-            </Route>
-              
+              </Route>
+
               <Route element={<GuestGuard />} >
                 <Route path='/login' element={<Login />} />
                 <Route path='/register' element={<Register />} />
               </Route>
 
               <Route path='/admin' element={
-                
-                  <Suspense fallback={<p>Loading...</p>}>
-                      <Admin />
-                  </Suspense>
-              }/>
-             
+
+                <Suspense fallback={
+                  <div className='flex items-center justify-center h-screen'>
+                    <p className='text-xl font-semibold text-blue-500 animate-pulse'>Loading...</p>
+                  </div>
+                }>
+                  <Admin />
+                </Suspense>
+              } />
+
             </Routes>
           </main>
-
+            <Footer />
         </div>
-  </UserProvider>
-      
+      </UserProvider>     
     </>
   )
 }
