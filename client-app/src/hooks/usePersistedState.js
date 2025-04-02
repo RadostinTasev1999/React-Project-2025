36c6ea7e199863b2b14f809c220fe2548d2af8ad
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-04-02 23:44:46 +0300

    update usePersistedState.js

--- client-app/src/hooks/usePersistedState.js
+++ client-app/src/hooks/usePersistedState.js
@@ -16,14 +16,14 @@ export default function usePersistedState(stateKey, inititalState) {
 
         const persistedStateData = JSON.parse(persistedState)
 
-        console.log('PersistedStateData is:', persistedStateData)
+       
         return persistedStateData
 
     });
 
         const setPersistedState = (input) => {
 
-            console.log('Input in setPersistedState is:', input)
+            
             
             const data = typeof input === 'function'
                     ?
@@ -32,10 +32,10 @@ export default function usePersistedState(stateKey, inititalState) {
                   input;
 
         const persistedData = JSON.stringify(data)
-        console.log(persistedData)
+       
         localStorage.setItem(stateKey, persistedData)
 
-        console.log('persistedData is:', persistedData)
+        
         /*
         {"email":"peter@abv.bg","username":"Peter","_id":"35c62d76-8152-4626-8712-eeb96381bea8","accessToken":"df495dd226a6e4c7c42c53158f3da6d5bcc98686c86bb2bdf8e82be5dd024b07"}
         */
