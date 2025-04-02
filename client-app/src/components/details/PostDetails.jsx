6c524b2334070e9943fc796dfd8bffcf9e02cf9f
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-04-02 23:40:08 +0300

    added conditional rendering in PostDetails

--- client-app/src/components/details/PostDetails.jsx
+++ client-app/src/components/details/PostDetails.jsx
@@ -13,7 +13,6 @@ export default function PostDetails(){
     const {postId} = useParams()
 
     const { post } = usePost(postId)
-    // http://localhost:3030/data/posts/0ac05014-08cc-4635-867a-cd87f28549be
     const { isAuthenticated } = useAuth()
     const { userId } = useAuth()
     const { deletePost } = useDeletePost()
@@ -21,71 +20,68 @@ export default function PostDetails(){
     
     const navigate = useNavigate()
 
-  console.log('isAuthenticated in PostDetails component:', isAuthenticated)
-    //console.log('Post is:',post)
-
     const isOwner = userId === post._ownerId
 
     const onDelete = async() => {
-       // event.preventDefault()
-        console.log('We are here')
+     
         await deletePost(postId)
-
-        console.log('Post successfully deleted!')
 
         navigate('/posts')
 
-        
     }
+
+
 
     return (
         <>
-         <div className="overflow-hidden bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-      <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-56 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-          <div className="lg:pt-4 lg:pr-8">
-            <div className="lg:max-w-lg">
-              {/* <h2 className="text-base/7 font-semibold text-indigo-600">{email}</h2> */}
-              <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">
-                {post.title}
-              </p>
-              <p className="mt-6 text-lg/8 text-gray-600">
-                {post.description}
-              </p>
-              
-            </div>
-          </div>
-          <img
-            alt="Product screenshot"
-            src={post.image}
-            
-            className="w-[14rem] max-w-none rounded-xl ring-1 shadow-xl ring-gray-400/10 sm:w-[28.5rem] md:-ml-4 lg:-ml-0"
-          />
-        </div>
-      </div>
-        {
-            isOwner ? (
-                        <div className="ml-30 mt-16 flex gap-4 justify-start">
-                            <Link to={`/posts/${postId}/edit`} className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                                Edit
-                            </Link>
-                            <Link onClick={onDelete} className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                                Delete
-                            </Link>
-                        </div>
-                ) : null    
-        }
-     <CommentsShow  />
-     {
-      isAuthenticated 
-            ?
-      <CommentsCreate/>
-            :
+        <div className="overflow-hidden w-[90%] m-auto bg-white py-24 sm:py-32">
+          <div className="bg-white shadow-lg pt-12 pb-12 rounded-lg border-1">
+            <div className="mx-auto max-w-7xl px-6 ml-26 lg:px-8">
+              <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-56 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+                <div className="lg:pt-4 lg:pr-8">
+                  <div className="lg:max-w-lg">
+                    {/* <h2 className="text-base/7 font-semibold text-indigo-600">{email}</h2> */}
+                    <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">
+                      {post.title}
+                    </p>
+                    <p className="mt-6 text-lg/8 text-gray-600">
+                      {post.description}
+                    </p>
 
-            null
-     }
-     
-      
+                  </div>
+                </div>
+                <img
+                  alt="Product screenshot"
+                  src={post.image}
+
+                  className="w-[14rem] max-w-none rounded-xl ring-1 shadow-xl ring-gray-400/10 sm:w-[28.5rem] md:-ml-4 lg:-ml-0"
+                />
+              </div>
+            </div>
+            {
+              isOwner ? (
+                <div className="ml-30 mt-16 flex gap-4 justify-start">
+                  <Link to={`/posts/${postId}/edit`} className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                    Edit
+                  </Link>
+                  <Link onClick={onDelete} className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                    Delete
+                  </Link>
+                </div>
+              ) : null
+            }
+          </div>
+          <CommentsShow />
+          {
+            isAuthenticated
+              ?
+              <CommentsCreate />
+              :
+
+              null
+          }
+
+
         </div>
         </>
     )
