de1c0fb970dc9e8586cb66f0419a7320f5941d0d
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-04-02 23:29:53 +0300

    update useComment hook and useDeleteComment hook

--- client-app/src/api/commentApi.js
+++ client-app/src/api/commentApi.js
@@ -3,6 +3,7 @@ import useAuth from "../hooks/useAuth"
 
 const baseUrl = 'http://localhost:3030/data/comments'
 
+
 export const useCreateComments = () => {
 
     const { request } = useAuth();
@@ -13,11 +14,7 @@ export const useCreateComments = () => {
             ...payload
         }
 
-        console.log('Comment data is:',commentData)
-
-        const newComment = request.post(baseUrl,commentData)
-
-        return newComment
+         request.post(baseUrl,commentData)
 
     }
 
@@ -27,8 +24,9 @@ export const useCreateComments = () => {
 
 
 }
-// create useComment hook
+
 export const useComments = (postId) => {
+
     const { request } = useAuth();
 
     const [comments, setComments] = useState({})
@@ -39,31 +37,21 @@ export const useComments = (postId) => {
             where: `postId="${postId}"`
         })
 
-        console.log("Search params are:", searchParams.toString())
-        /*
-        URLSearchParams - defines utility methods to work with the query string of a URL.
-
-        */
         request.get(`${baseUrl}?${searchParams.toString()}`)
             .then((result) => setComments(result))
+
     },[postId,request])
 
     return {
         comments
     }
 
-    /*
-    where=%7B%22postId%22%3A%22859e8be3-e7cf-4c76-9e36-86f57610653a%22%7D&load=author%3D_ownerId%3A+users
-    */
-        
-
 }
 
-// edit comment
 
 export const useEditComment = () => {
 
-    const {request} = useAuth()
+    const { request } = useAuth()
 
     const edit = (commentId,payload) => {
 
@@ -77,16 +65,15 @@ export const useEditComment = () => {
     }
 }
 
-// delete comment
 
 export const useDeleteComment = () => {
+
     const { request } = useAuth();
 
     const deleteComment = (commentId) => {
 
-        const requestDelete = request.delete(`${baseUrl}/${commentId}`)
-          
-        return requestDelete
+         request.delete(`${baseUrl}/${commentId}`)
+      
     }
 
     return {
@@ -96,24 +83,22 @@ export const useDeleteComment = () => {
 }
 
 
-// To fix:
-export const useComment = () => {
+export const useComment = (commentId) => {
+
     const { request } = useAuth()
 
-    const [postComment,setPostComment] = useState({})
+    const [postComment,setPostComment] = useState('')
 
-    const getComment = (commentId) => {
+    useEffect(() => {
 
-        useEffect(() => {
-            request.get(`${baseUrl}/${commentId}`)
-                 .then((result) => setPostComment(result))
-        },[commentId,request])
-            
-        return postComment
-    }
+         request.get(`${baseUrl}/${commentId}`)
+            .then((result) => setPostComment(result))
+      
+    },[commentId,request])
+  
 
     return {
-        getComment
+        postComment
     }
 
 }
