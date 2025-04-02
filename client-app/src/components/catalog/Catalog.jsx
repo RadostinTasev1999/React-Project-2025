af4ab57986fccd614a02a0a0011f9864391d496f
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-04-02 23:34:59 +0300

    Updated css in Catalog.jsx

--- client-app/src/components/catalog/Catalog.jsx
+++ client-app/src/components/catalog/Catalog.jsx
@@ -1,27 +1,18 @@
-// import { usePosts } from "../../api/postApi"
 import { formattedDate } from "../../utils/date"
-
 import { Link } from "react-router"
-
 import { usePosts } from "../../api/postApi"
 
 export default function Catalog() {
 
-    const {posts } = usePosts()
-    // const { email } = useAuth()
-
-   
-    console.log('Posts are:', posts)
-
-   
+    const { posts } = usePosts() 
 
     return (
         <>
             <div className="bg-white py-24 sm:py-32">
                 <div className="mx-auto max-w-7xl px-6 lg:px-8">
                     <div className="mx-auto max-w-2xl lg:mx-0">
-                        <h2 className="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">Latest posts</h2>
-                        <p className="mt-2 text-lg/8 text-gray-600">Learn the latest product feedback and features from the community hub.</p>
+                        <h2 className="text-4xl font-extrabold tracking-tight text-white bg-gradient-to-r from-teal-500 via-blue-500 to-purple-500 sm:text-5xl italic shadow-lg p-6 rounded-lg text-center max-w-3xl mx-auto">Latest posts</h2>
+                        <p className="mt-4 text-lg text-gray-700 sm:text-xl italic tracking-tight leading-relaxed max-w-3xl mx-auto">Learn the latest product feedback and features from the community hub.</p>
                     </div>
                     <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
                         {posts.map((post) => (
@@ -30,7 +21,7 @@ export default function Catalog() {
                                     <time dateTime={formattedDate(posts._createdOn)} className="text-gray-500">
                                         {formattedDate(posts._createdOn)}
                                     </time>
-                                    
+
                                 </div>
                                 <div className="group relative">
                                     <h3 className="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-gray-600">
@@ -44,10 +35,10 @@ export default function Catalog() {
                                 <div className="relative mt-8 flex items-center gap-x-4">
                                     <img alt="Image" src={post.image} className="size-50  w-82 h-32" />
                                 </div>
-                                <Link to={`/posts/${post._id}/details`}className="px-4 py-1.5 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 mt-2">See post</Link>
+                                <Link to={`/posts/${post._id}/details`} className="px-4 py-1.5 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 mt-2">See post</Link>
 
                             </article>
-                            
+
                         ))}
                     </div>
                 </div>
