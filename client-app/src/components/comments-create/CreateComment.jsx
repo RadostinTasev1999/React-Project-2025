9e453cbc0a94aff73c2175647b02eb4f94727325
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-04-08 08:06:11 +0300

    passed form action handler from parent component via props

--- client-app/src/components/comments-create/CreateComment.jsx
+++ client-app/src/components/comments-create/CreateComment.jsx
@@ -1,94 +1,11 @@
-import { useCreateComments } from "../../api/commentApi"
-import {  useParams } from "react-router"
-import useAuth from "../../hooks/useAuth"
-import { v4 as uuid } from 'uuid'
-import { useState } from "react"
-
-export default function CommentsCreate() {
-
-    const { postId } = useParams()
-    const {create} = useCreateComments()
-    const { userId } = useAuth()
-    const { email } = useAuth()
-
-    const [formData,setFormData] = useState({
-        
-        username: '',
-        comment:''
-        
-      })
-    
-      // create errors state property
-      const [errors,setErrors] = useState({})
-    
-      const handleChange = (event) => {
-        event.preventDefault()
-    
-        const { name,value } = event.target
-    
-        setFormData((prevState) => ({
-          ...prevState,
-          [name]: value
-        }))
-    
-      }
-    
-    
-      // create validation function
-    
-      const validateForm = (data) => {
-    
-        let errors = {};
-       
-    
-        if (!data.username.trim()) {
-          errors.username = 'Username is required'
-        }
-    
-        if (!data.comment.trim()) {
-            errors.comment = 'Comment is required'
-        } else if(data.comment.length < 5 ){
-          errors.comment = 'Comment must be at least  5 characters'
-        }
-    
-        return errors
-      }
-    
-      // implement email validation logic
-
-    const onCreateComment = (formData) => {
-
-        const { username, comment} = Object.fromEntries(formData)
-
-        const validationErrors = validateForm({username,comment})
-
-        if (Object.keys(validationErrors).length === 0) {
-            const payload = {
-                _id:uuid(),
-                username,
-                comment,
-                _ownerId:userId,
-                postId,
-                author:{
-                    email
-                }
-            }
-    
-            create(payload)
-    
-            window.location.reload()
-        }else {
-            setErrors(validationErrors)
-        }
-
-       
-
-    }
+export default function CommentsCreate(
+    {createHandler}
+) {
 
 
     return (
         <>
-            <form action={onCreateComment} className="w-1/2 ml-36 pt-12 p-8 bg-white shadow-lg rounded-lg border-2">
+            <form action={createHandler} className="w-1/2 ml-36 pt-12 p-8 bg-white shadow-lg rounded-lg border-2">
                 <div className="space-y-12">
                     <div className="border-b border-gray-900/10 pb-12">
                         <h2 className="text-base/7 font-semibold text-gray-900">Post a Comment</h2>
@@ -109,13 +26,12 @@ export default function CommentsCreate() {
                                             name="username"
                                             type="text"
                                             placeholder="Enter Username"
-                                            onChange={handleChange}
-                                            value={formData.username}
+                                            
                                             className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
                                         />
                                         
                                     </div>
-                                    {errors && <span className="border-red-500 bg-red-100 italic">{errors.username}</span>}
+                                    {/* {errors && <span className="border-red-500 bg-red-100 italic">{errors.username}</span>} */}
                                 </div>
                             </div>
 
@@ -127,13 +43,13 @@ export default function CommentsCreate() {
                                     <textarea
                                         id="comment"
                                         name="comment"
-                                        onChange={handleChange}
-                                        value={formData.comment}
+                                        // onChange={handleChange}
+                                        // value={formData.comment}
                                         rows={3}
                                         className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
 
                                     />
-                                    {errors && <span className="border-red-500 bg-red-100 italic">{errors.comment}</span>}
+                                    {/* {errors && <span className="border-red-500 bg-red-100 italic">{errors.comment}</span>} */}
                                 </div>
 
                             </div>
