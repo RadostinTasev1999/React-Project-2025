d3edc7e91f5d3a81282dd72a916844fbc52e4c0c
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-04-08 08:14:02 +0300

    Miscellaneous updates

--- client-app/src/contexts/UserContext.jsx
+++ client-app/src/contexts/UserContext.jsx
@@ -1,5 +1,13 @@
 import { createContext, useContext } from 'react';
 
+
+/*
+if you call useContext(UserContext) in a component without a Provider,
+it will use this object.
+
+We wrap the App with a <UserContext.Provider> and provide the actual
+user data and functions.
+*/
 export const UserContext = createContext({
     _id: '',
     email: '',
@@ -8,6 +16,12 @@ export const UserContext = createContext({
     userLoginHandler: () => null,
     userLogoutHandler: () => null
 });
+
+/*
+createContext() - is a React function that creates a Context object
+A Context allows you to share data between components without having to pass
+props manually through the components in the component tree
+*/
 /*
 Here we define the shared data that we want to provide across the component tree.
 
--- client-app/src/hooks/useAuth.js
+++ client-app/src/hooks/useAuth.js
@@ -2,6 +2,7 @@ import { useContext,useMemo,useCallback } from "react";
 import { UserContext } from "../contexts/UserContext";
 import request from "../utils/request";
 
+// Custom React Hook
 export default function useAuth() {
 
     const { 
@@ -9,6 +10,8 @@ export default function useAuth() {
         email,
         _id,
      } = useContext(UserContext)
+
+     // retrieve user authentication through UserContext object
 
     const requestWrapper = useCallback((method,url, data, options = {}) => {
 
@@ -20,12 +23,24 @@ export default function useAuth() {
                     ...options.headers
                 }
             };
+            /*
+                We create a new authOption object that merges the provided options
+                Add a custom X-Authorization header with the user's token if available.
 
-            
+            */
 
             return request.baseRequest(method, url, data, accessToken ? authOption : options);
-            
+            /*
+                we call baseRequest which is a returned property from request method.
+                if accessToken exists, the request includes the auth headers property
+                otherwise, it falls back to options without adding X-Authorization
+            */
     }, [accessToken])
+
+    /*
+    Creates a memoized function to make HTTP requests:
+
+    */
 
     const requestObject = useMemo(() => ({
         get: requestWrapper.bind(null,'GET'),
@@ -34,6 +49,13 @@ export default function useAuth() {
         patch:requestWrapper.bind(null,'PATCH'),
         delete: requestWrapper.bind(null,'DELETE')
     }),[requestWrapper])
+
+    // builds an object with funciton for each HTTP method
+    // each function is pre-configured to use a specific method
+    // useMemo:  
+    // - React hook returns a memoized value
+    // - memoization is caching a value so that it does not need to be recalculated     
+    // - the hook only runs when one of its dependencies updates (requestWrapper)   
 
     return {
         accessToken,
