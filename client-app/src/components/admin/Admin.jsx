7710c040ee47c8675d459b11ca8e2f1ed4406789
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-04-08 08:05:12 +0300

    Rendered AdminComments component

--- client-app/src/components/admin/Admin.jsx
+++ client-app/src/components/admin/Admin.jsx
@@ -1,29 +1,18 @@
-import { Component } from "react"
-import { UserContext } from "../../contexts/UserContext"
-import {ErrorBoundary} from "react-error-boundary"
-
+import React, { Component } from "react";
+import AdminComments from "./admin-comments/AdminComments";
 
 export default class Admin extends Component {
-    render () {
-
+    render(){
         return (
-            <ErrorBoundary>
-                <UserContext.Consumer>
-                    {(context) => {
-                       
-                       return (<div className="card shadow-lg max-w-xs mx-auto text-center font-sans bg-white p-6 rounded-lg transform transition-transform hover:scale-105">
-                        <img src="https://www.kindpng.com/picc/m/21-211456_user-icon-hd-png-download.png" alt="John" className="w-32 h-32 rounded-full mx-auto mb-4 border-4 border-gray-300"/>
-                        <h1 className="title text-gray-800 text-2xl font-semibold mb-2">{context.email}</h1>
-                        <p className="text-gray-600 text-sm mb-2">{context.username}</p>
-                        <p className="text-gray-500 text-xs mb-6">Member</p>         
-                        </div>
-                       )   
-                }}
-                </UserContext.Consumer>
-            </ErrorBoundary>
+            <>
+                <h1>Admin Panel</h1>
+                <AdminComments />
+            </>         
         )
     }
 }
 
 
+/*
 
+*/
