fd2708087c725de5f5eb72229fb64d3683d91595
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-04-08 08:12:35 +0300

    Updated CommentsShow

--- client-app/src/components/comments-show/CommentsShow.jsx
+++ client-app/src/components/comments-show/CommentsShow.jsx
@@ -1,19 +1,20 @@
 import { useParams } from "react-router"
-import { useComments } from "../../api/commentApi"
 import { formattedDate } from "../../utils/date"
 import useAuth from "../../hooks/useAuth"
 import { Link } from "react-router"
 import { useDeleteComment } from "../../api/commentApi"
 
 
-export default function CommentsShow(){
+export default function  CommentsShow(
+    {comments}
+){
 
     const { postId } = useParams()
     const { userId } = useAuth()
     const { deleteComment } = useDeleteComment()
-    
 
-   const {comments} = useComments(postId)
+    console.log('Comments are:', comments)
+    
 
    const onDelete = async(commentId) => {
 
@@ -42,19 +43,24 @@ export default function CommentsShow(){
 
                 {
                     comments.length > 0 ?
-                        (
-                            comments.map((comment) => {
-                                return <div key={comment._id} className="bg-gray-100 p-4 rounded-md shadow-sm mt-8 max-w-2xl">
-                                    <div className="flex justify-between items-center mb-2">
-                                        <span className="text-sm font-semibold  italic text-blue-500">{comment.username} </span>
+                        
+                            comments.map((comment) => (
+                                 <div key={comment._id} className={`bg-gray-100 p-4 rounded-md shadow-sm mt-8 max-w-2xl ${comment.pending ? 'border border-yellow-300 bg-yellow-100 text-yellow-800' : ''}`}>                                   
+                                        <span className="text-sm font-semibold  italic text-blue-500">
+                                            <p className="text-sm text-gray-600 font-sm italic"><b>User:</b> {comment.username}</p>
+                                        </span>
+                                    
+                                    <span className="text-gray-700 mt-4 mb-2">
+                                        <p className="text-sm text-gray-600 font-sm italic"><b>UserId:</b> {comment.author?._id}</p>
+                                        <p className="text-sm text-gray-600 font-medium italic"><b>Email:</b> {comment.author?.email}</p>
                                         <span className="text-xs text-gray-500">{formattedDate(comment._createdOn)}</span>
-                                    </div>
-                                    <p className="text-gray-700">{comment.comment}</p>
-
+                                        <p className="mt-10"> {comment.comment}</p>
+                                    </span>
+                                  
                                     {
                                         comment._ownerId === userId
                                             ?
-                                            <div className="mt-6 flex items-center justify-end gap-x-6">
+                                            <div className="mt-2 flex items-center justify-end gap-x-6">
 
                                                 <Link
                                                     to={`/posts/${postId}/comment/${comment._id}`}
@@ -75,8 +81,7 @@ export default function CommentsShow(){
 
 
                                 </div>
-                            })
-                        )
+                            ))
                         :
                         <p className="text-blue-500 font-semibold text-sm flex items-center space-x-2 italic" >
                             <span className="text-gray-500 text-sm font-medium p-2 bg-gray-100 rounded-md">No comments on this post yet...</span>
